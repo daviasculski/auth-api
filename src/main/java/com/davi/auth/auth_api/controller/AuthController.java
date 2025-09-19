f8f7d406bca13833850cece5cbda70c3433fc7b6
Davi Iasculski <davikolski@gmail.com>
2025-09-18 21:13:26 -0300

    feat(auth): Adiciona a funcionalidade de registro de usuário, com a integração do controller, service e entity.

---
+++ src/main/java/com/davi/auth/auth_api/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.davi.auth.auth_api.controller;
+
+import com.davi.auth.auth_api.dto.UserRegistrationDTO;
+import com.davi.auth.auth_api.entity.User;
+import com.davi.auth.auth_api.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody UserRegistrationDTO registrationDTO){
+        User registeredUser=userService.registerUser(registrationDTO.getUsername(), registrationDTO. getPassword() );
+        return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);    
+    }
+}
--- src/test/java/com/davi/auth/auth_api/AuthApiApplicationTests.java
+++ src/test/java/com/davi/auth/auth_api/AuthApiApplicationTests.java
@@ -2,8 +2,11 @@ package com.davi.auth.auth_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthApiApplicationTests {
 
 	@Test
